7e730590f7590f5feb1ebb82b89c7006f6d9c723
AbdouVak <abdou.vakouiev@gmail.com>
2023-05-04 13:33:17 +0200

    ajout: de "total general" pour connaitre le prix de tout les produits

--- recap.php
+++ recap.php
@@ -27,19 +27,24 @@
                         "</tr>",
                     "</thread>",
                     "<tbody>";
-                    //boucle itérative foreach efficace pour exécuter, produit par produit
-                    foreach($_SESSION['products'] as $index => $products){
-                        echo "<tr>",
-                                "<td>".$index."</td>",
-                                "<td>".$products['name']."</td>",
-                                //fonction number_format() permet de modifier l'affichage d'une valeur numérique
-                                "<td>".number_format($products['price'],2,",","&nsbp;")." €</td>",
-                                "<td>".$products['qtt']."</td>",
-                                "<td>".number_format($products['total'],2,",","&nsbp;")." €</td>",
-                              "</tr>";
-                    }
-            echo    "</tbody>",
-                "</table>";
+                $totalGeneral = 0;
+                //boucle itérative foreach efficace pour exécuter, produit par produit
+                foreach($_SESSION['products'] as $index => $products){
+                    echo "<tr>",
+                            "<td>".$index."</td>",
+                            "<td>".$products['name']."</td>",
+                            //fonction number_format() permet de modifier l'affichage d'une valeur numérique
+                            "<td>".number_format($products['price'],2,",","&nsbp;")." €</td>",
+                            "<td>".$products['qtt']."</td>",
+                            "<td>".number_format($products['total'],2,",","&nsbp;")." €</td>",
+                          "</tr>";
+                          $totalGeneral =$products['total'];
+                }
+                echo "<tr>",
+                        "<td colspan=4>Total général:</td>",
+                        "<td><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
+                    "</tbody>",
+                 "</table>";
         }
     ?>
 </body>
