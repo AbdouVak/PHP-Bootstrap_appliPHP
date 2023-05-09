7022eb98359e0d541bf3e69ee50889bd330176a4
AbdouVak <abdou.vakouiev@gmail.com>
2023-05-09 13:36:28 +0200

    amelioration ésthétique de la page

--- recap.php
+++ recap.php
@@ -28,7 +28,9 @@
         //soit clé existe mais ne contient aucune donnée : empty()
         if(!isset($_SESSION['products'])||empty($_SESSION['products'])){
             //affiche à utilisateur message prévenant aucun produit existe en session
-            echo "<p>Aucun produit en session...</p>";
+            echo "<div class='container-sm d-flex flex-row justify-content-around bg-warning rounded-4 shadow mt-5'>
+                    <p class='fs-2 text-secondary mt-3 mb-3 ps-3'>Aucun produit en session...</p>
+                </div>";
         }else{
             echo "<div class='container-sm bg-warning  rounded-4 shadow mt-5'><table class='table'>",
                     "<thread ",
@@ -38,7 +40,7 @@
                             "<th class='fs-2 text-secondary'>Prix</th>",
                             "<th class='fs-2 text-secondary'>Quantité</th>",
                             "<th class='fs-2 text-secondary'>Total</th>",
-                            "<th class='fs-2 text-secondary'>Delete</th>",
+                            "<th class='fs-2 text-secondary'>Delete  products</th>",
                         "</tr>",
                     "</thread>",
                     "<tbody>";
@@ -51,14 +53,14 @@
                             //fonction number_format() permet de modifier l'affichage d'une valeur numérique
                             "<td class='fs-2 text-secondary'>".number_format($product['price'],2,",","&nsbp;")." €</td>",
                             "<td class='fs-2 text-secondary'>
-                                <a href='traitement.php?action=qttMinus&id=$index'>-</a>".
+                                <a href='traitement.php?action=qttMinus&id=$index' class='text-muted text-decoration-none'> - </a>".
                                 $product['qtt'].
-                                "<a href='traitement.php?action=qttPlus&id=$index'>+</a>".
+                                "<a href='traitement.php?action=qttPlus&id=$index'class='text-muted text-decoration-none'> + </a>".
                             "</td>",
                             
                             "<td class='fs-2 text-secondary'>".number_format($product['total'],2,","," ")." €</td>",
                             "<td class='fs-2 text-secondary'>".
-                                "<a href='traitement.php?action=delete&id=$index'>Delete</a>".
+                                "<a href='traitement.php?action=delete&id=$index' class='text-muted text-decoration-none'>Delete</a>".
                             "</td>",
                         "</tr>";
                           $totalGeneral +=$product['total'];
@@ -67,7 +69,7 @@
                         "<td class='fs-2 text-secondary' colspan=4>Total général:</td>",
                         "<td class='fs-2 text-secondary'><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
                         "<td class='fs-2 text-secondary'>".
-                                "<a href='traitement.php?action=deleteAll'>DeleteALL</a>".
+                                "<a href='traitement.php?action=deleteAll' class='text-muted text-decoration-none' >DeleteALL</a>".
                         "</td>",
                         "</tbody>",
                  "</table></div>";
