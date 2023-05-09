012e8aedf373426bc7f841cbd519774b9556e175
AbdouVak <abdou.vakouiev@gmail.com>
2023-05-09 11:34:09 +0200

    ajout de la fonctionalité pour voir le nbr de produit juste a coté du panier

--- recap.php
+++ recap.php
@@ -18,16 +18,12 @@
 
             <button type="button" onclick="location.href='recap.php'" class="btn btn-warning position-relative">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16"><path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z"/></svg>
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark"> <span class="visually-hidden">unread messages</span></span>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark"> <?php echo count($_SESSION['products'])?><span class="visually-hidden">unread messages</span></span>
             </button>
 
         </nav>
         <h1 class="d-flex justify-content-center  text-warning mt-5">Votre panier</h1>
     <?php 
-        function minus(){
-
-        }
-
         //vérifie clé "products" du tableau $_SESSION n'existe pas : !isset()
         //soit clé existe mais ne contient aucune donnée : empty()
         if(!isset($_SESSION['products'])||empty($_SESSION['products'])){
@@ -62,7 +58,7 @@
                             
                             "<td class='fs-2 text-secondary'>".number_format($product['total'],2,","," ")." €</td>",
                             "<td class='fs-2 text-secondary'>".
-                                "<a href='traitement.php?action=delete'>Delete</a>".
+                                "<a href='traitement.php?action=delete&id=$index'>Delete</a>".
                             "</td>",
                         "</tr>";
                           $totalGeneral +=$product['total'];
