60cf67393a1faa46685bcb7bf43f4597902d3d30
AbdouVak <abdou.vakouiev@gmail.com>
2023-05-11 12:02:22 +0200

    verifier verification si files contient bien une image

--- traitement.php
+++ traitement.php
@@ -6,14 +6,14 @@
             case "ajoutProduit" :
                     // limite accès à traitement.php par les requêtes HTTP venant de la soumission du formulaire
                     if(isset($_POST['submit'])){
-
                         //filtre supprime chaîne caractères qui contient caractères spéciaux et balise HTML
                         $name=filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING);
                         //validera le prix si c'est nombre à virgule / permet utilisation du caractère "," ou "." pour la décimale
                         $price=filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION);
                         //validera la quantité que si c'est un nombre entier différent de zéro
                         $qtt=filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT);
-
+                        
+                    
                         //vérifie implicitement si chaque variable contient valeur jugée positive par PHP 
                         if($name && $price>0 && $qtt>0){
                             //tableau associatif $product
@@ -29,9 +29,43 @@
                         }else{
                             $_SESSION['message'] = "<p class='fs-2 text-secondary'>Le produit n'a pas était ajouté</p>";
                         }
-                        
                     }
-                    header("Location:index.php");
+                    // verifie si il contient des images
+                    if(isset($_FILES['file'])){
+                        echo "image";
+                        $tmpName = $_FILES['file']['tmp_name'];
+                        //stock le nom de l'image
+                        $name = $_FILES['file']['name'];
+                        //stock la valeur de la taille de l'image
+                        $size = $_FILES['file']['size'];
+                        //stock le numero de l'erreur de l'image
+                        $error = $_FILES['file']['error'];
+                        $tabExtension = explode('.', $name);
+
+                        //change l'extension en miniscule
+                        $extension = strtolower(end($tabExtension));
+                        //crée une table avec les extention
+                        $extensions = ['jpg', 'png', 'jpeg', 'gif'];
+                        //Taille max que l'on accepte
+                        $maxSize = 400000;
+
+                        //verifie si les extention sont correct,que la taille est en dessous de la taille max et qu'il n'y a pas d'erreur
+                        if(in_array($extension, $extensions) && $size <= $maxSize && $error == 0){
+                            $uniqueName = uniqid('', true);
+                            //uniqid génère quelque chose comme ca : 5f586bf96dcd38.73540086
+                            $file = $uniqueName.".".$extension;
+                            //$file = 5f586bf96dcd38.73540086.jpg
+                            move_uploaded_file($tmpName, './upload/'.$file);
+                            $_SESSION['image'] = $file;
+                        }
+                        else{
+                            echo "Une erreur est survenue";
+                        }
+                        header("Location:index.php");  
+                    }
+                    
+
+                    
                     break;
 
             case "qttMinus" :
@@ -44,24 +78,20 @@
                 break;
 
             case "qttPlus" :
-
                 $_SESSION['products'][$_GET['id']]['qtt']++;
                 $_SESSION['products'][$_GET['id']]['total'] = $_SESSION['products'][$_GET['id']]['qtt'] * $_SESSION['products'][$_GET['id']]['price'];
                 header("Location:recap.php");
                 break;    
             
             case "delete" :
-
                 unset($_SESSION['products'][$_GET['id']]);
                 header("Location:recap.php");
                 break;
 
             case "deleteAll" :
-                
                 foreach($_SESSION['products'] as $index => $product){
                     unset($_SESSION['products'][$index]);
                 }
-                
                 break;
         }
     }
