58543bf6fdcf4347c3aeee9e9b148eb49ea2c8d0
AbdouVak <abdou.vakouiev@gmail.com>
2023-05-11 12:00:45 +0200

    correcction: le file est bien envoyé aux traitement

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     ob_start();
 ?>
 
-    <form action="traitement.php?action=ajoutProduit" method="post" class=" container-sm d-flex flex-row justify-content-around bg-warning rounded-4 shadow mt-5">
+    <form action="traitement.php?action=ajoutProduit" method="POST" class=" container-sm d-flex flex-row justify-content-around bg-warning rounded-4 shadow mt-5" enctype="multipart/form-data">
     <div class="">
         
         <div class="d-flex justify-content-around flex-row  bg-secondary mt-5 mb-5 shadow rounded-2">
@@ -35,38 +35,15 @@
             </p>
         </div>
 
-        <?php 
-            if(isset($_FILES['file'])){
-                $tmpName = $_FILES['file']['tmp_name'];
-                $name = $_FILES['file']['name'];
-                $size = $_FILES['file']['size'];
-                $error = $_FILES['file']['error'];
-                $tabExtension = explode('.', $name);
-
-                $extension = strtolower(end($tabExtension));
-
-                $extensions = ['jpg', 'png', 'jpeg', 'gif'];
-                //Taille max que l'on accepte
-                $maxSize = 400000;
-                if(in_array($extension, $extensions) && $size <= $maxSize && $error == 0){
-                    move_uploaded_file($_SESSION['image']);
-                }
-                else{
-                    echo "Une erreur est survenue";
-                }
-            }
-        ?>
-
         <div class="d-flex justify-content-Center  align-items-center flex-row  bg-secondary mt-5 mb-5 p-1 shadow rounded-2">
-            <label for="file"class="fs-2 text-warning mt-3 mb-3 ps-3"> Image produit :</label>
+            <label for="file" class="fs-2 text-warning mt-3 mb-3 ps-3"> Image produit :</label>
             <input type="file" name="file" class="pe-2 ps-2 text-warning">
-            <button type="submit"class="fs-2 bg-warning text-secondary border border-0 rounded-5 mt-3 mb-3 p-2 shadow-lg">Enregistrer</button>
         </div>
-
+        
     </div>
 
     <div class="d-flex justify-content-center align-items-center flex-column  mt-5">
-        <p >
+        <p>
             <input type="submit"  name="submit" value="Ajouter le produit" class="fs-2 bg-secondary text-white border border-0 rounded-5 p-3 shadow-lg ">
         </p>
 
@@ -75,14 +52,13 @@
             if(isset($_SESSION['message'])){
                 echo $_SESSION['message'];
                 unset( $_SESSION['message']);
-            }  
+            }
             ?>
         </p>
-        
     </div>
-
     </form>
+
     <?php
-$titre = "Ajout produit";
+    $titre = "Ajout produit";
     $contenu = ob_get_clean();
     require "template.php";
